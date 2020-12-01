c995d0039b7994800ee8676638642a4ffe28c7da
Aleksander Panov <i.miroque@gmail.com>
2020-12-01 23:35:18 +0700

    :construction: #2 in proccess... finding how to test

--- src/main/java/ru/miroque/fstorage/ApiFStorage.java
+++ src/main/java/ru/miroque/fstorage/ApiFStorage.java
@@ -2,10 +2,8 @@ package ru.miroque.fstorage;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -27,5 +25,24 @@ public class ApiFStorage {
         log.info("<- getItemByUuid::{}", uuid);
         return ResponseEntity.ok(item.orElseThrow(() -> new NotFoundException("File with uuid::" + uuid + " not found")));
     }
+
+    @PostMapping("/")
+    public ResponseEntity<UUID> handleFileUpload(@RequestParam MultipartFile file) {
+        log.info("-> handleFileUpload");
+        File item = new File();
+        item.setMime(file.getContentType());
+        item.setName(file.getOriginalFilename());
+        item.setPinned(Boolean.FALSE);
+        item.setUuid(UUID.randomUUID());
+        item = rFile.saveAndFlush(item);
+        log.info("<- handleFileUpload::id::{}::uuid::{}",item.getId(),item.getUuid());
+        return ResponseEntity.ok(item.getUuid());
+    }
+
+    //COPY-PASTE:: https://spring.io/guides/gs/uploading-files/
+    /*@ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(NotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }*/
 
 }
